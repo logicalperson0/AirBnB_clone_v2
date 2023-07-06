2819539dcb43fc2a96f43c8d813552944d30b378
Samuel <a2jsamuel@gmail.com>
2023-07-06 21:30:39 +0300

    This is to fix 2-file for deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,14 +7,16 @@ archive to the web servers
 from fabric.api import *
 from datetime import datetime
 import os
-import shlex
+# import shlex
 
 
 env.hosts = ["54.85.96.179", "34.232.65.41"]
 
-"""
+
 def do_deploy(archive_path):
+    """
     distributes an archive to the web servers
+    """
     if not os.path.exists(archive_path):
         return False
 
@@ -37,33 +39,4 @@ def do_deploy(archive_path):
         return True
 
     except Exception:
-        return False
-"""
-def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except:
         return False
