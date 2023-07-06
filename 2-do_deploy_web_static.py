bf525a02ae434489a3dec0a876aa191770a40395
Samuel <a2jsamuel@gmail.com>
2023-07-06 21:12:31 +0300

    this is to fix and add!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,16 +6,13 @@ archive to the web servers
 
 from fabric.api import *
 from datetime import datetime
-import os.path
+import os
+import shlex
 
 
 env.hosts = ["54.85.96.179", "34.232.65.41"]
 
-
 def do_deploy(archive_path):
-    """
-    distributes an archive to the web servers
-    """
     if not os.path.exists(archive_path):
         return False
 
@@ -26,17 +23,15 @@ def do_deploy(archive_path):
         pathing = '/data/web_static/releases/'
 
         put(archive_path, '/tmp/')
-
         run('mkdir -p {}{}/'.format(pathing, no_tgz))
-        run('tar -xzf /tmp/{} -C {}{}'.format(name_arc, pathing, no_tgz))
-        run('rm -f /tmp/{}'.format(name_arc))
-
-        run('mv {}{}/web_static/* {}{}/'.format(pathing, no_tgz))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(name_arc, pathing, no_tgz))
+        run('rm /tmp/{}'.format(name_arc))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(pathing, no_tgz))
         run('rm -rf {}{}/web_static'.format(pathing, no_tgz))
-
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(pathing, no_tgz))
 
+        print("New version deployed!")
         return True
 
     except Exception:
