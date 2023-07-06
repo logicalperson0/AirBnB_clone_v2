1a2db8eb8f34460ce47d86e6c52e6db41f733049
Samuel <a2jsamuel@gmail.com>
2023-07-06 16:32:41 +0300

    Add 1-file for deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,13 +2,13 @@
 """
 Function that compress a folder
 """
-"""
 from fabric.api import local
 from datetime import datetime
 import os.path
 
 
 def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
     try:
         if not os.path.exists("versions"):
             local("mkdir versions")
@@ -21,21 +21,4 @@ def do_pack():
         return f_name
 
     except Exception:
-        return None
-"""
-
-
-from fabric.api import local
-import time
-
-
-def do_pack():
-    """Generate an tgz archive from web_static folder"""
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
         return None
