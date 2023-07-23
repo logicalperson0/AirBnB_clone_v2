914815141b74f096a563bd7a4356bcb8e517f03f
Samuel <a2jsamuel@gmail.com>
2023-07-24 01:16:04 +0300

    This is for task 9!

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_app(exception=None):
+    """teardown_app"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """/states_list: display a HTML page"""
+    states_lists = storage.all(State).values()
+    # sorted(entries, key=lambda d: d['title'])
+    states_lists = sorted(states_lists, key=lambda d: d.name)
+
+    return render_template("7-states_list.html", states_lists=states_lists)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
