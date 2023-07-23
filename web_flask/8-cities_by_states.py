3b2eabd8785b67748c5895471d6de7a77aafafcb
Samuel <a2jsamuel@gmail.com>
2023-07-24 01:20:25 +0300

    Fix and add 8.py-file!

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,14 +17,16 @@ def teardown_app(exception=None):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     """/states_list: display a HTML page"""
     states_lists = storage.all(State).values()
     # sorted(entries, key=lambda d: d['title'])
     states_lists = sorted(states_lists, key=lambda d: d.name)
 
-    return render_template("7-states_list.html", states_lists=states_lists)
+    return render_template("8-cities_by_states.html",
+                           states_lists=states_lists)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
