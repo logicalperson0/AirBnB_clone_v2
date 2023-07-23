3c2c0e98aea9e75626656f24265ebf1503279f37
Samuel <a2jsamuel@gmail.com>
2023-07-24 01:42:44 +0300

    ADD AND FIX

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 """
-Starts a Flask we application
+script that starts a Flask web application
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db_session(exception):
-    """ close storage session after each request """
+def teardown_app(exception=None):
+    """teardown_app"""
     storage.close()
 
 
-@app.route('/cities_by_states')
-def get_states_list():
-    """ renders jinja2 template with states variable. """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states_lists)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """/states_list: display a HTML page"""
+    states_lists = storage.all(State).values()
+    # sorted(entries, key=lambda d: d['title'])
+    states_lists = sorted(states_lists, key=lambda d: d.name)
+
+    return render_template("8-cities_by_states.html",
+                           states_lists=states_lists)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     storage.reload()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
